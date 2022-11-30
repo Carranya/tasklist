a4cb42fb3dd5c8f6b527216a9934e41d7c31fc01
Carranya <k.gong82@gmail.com>
2022-11-30 08:26:12 +0100

    add sample data

---
+++ data.php
@@ -0,0 +1,6 @@
+<?php
+$data = [
+    ['id' => 1, 'task' => 'creating', 'date' => '2022-30-11', 'active' => 1],
+    ['id' => 2, 'task' => 'sample', 'date' => '2022-25-11', 'active' => 1],
+    ['id' => 2, 'task' => 'testing', 'date' => '2022-20-11', 'active' => 1],
+];
