018b743fcd07ff1c51e226116738a454ae2dacca
Carranya <k.gong82@gmail.com>
2022-12-02 06:43:28 +0100

    new data sample

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
 $data = [
-    ['id' => 1, 'task' => 'creating', 'date' => '2022-30-11', 'active' => 1],
-    ['id' => 2, 'task' => 'sample', 'date' => '2022-25-11', 'active' => 1],
-    ['id' => 2, 'task' => 'testing', 'date' => '2022-20-11', 'active' => 1],
+    ['id' => 1, 'task' => 'creating', 'date' => '2022-11-25', 'active' => 1],
+    ['id' => 2, 'task' => 'sample', 'date' => '2022-11-15', 'active' => 1],
+    ['id' => 3, 'task' => 'testing', 'date' => '2022-11-08', 'active' => 0],
 ];
