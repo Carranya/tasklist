700d137db5d19146feeb87ea362b70e585dabb9c
Carranya <k.gong82@gmail.com>
2022-12-04 06:56:36 +0100

    add create button

--- pages/main.php
+++ pages/main.php
@@ -1,5 +1,10 @@
 <form action='index.php' method='post'>
 <?php
+
+if(isset($_POST['create'])){
+    create($_POST['newTask']);
+}
+    require('data.php');
     include 'pages/showList.php';
 ?>
 </form>
