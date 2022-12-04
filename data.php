14717679f5ee2be21dceb5bec1edaec62bae1801
Carranya <k.gong82@gmail.com>
2022-12-04 06:55:40 +0100

    new data.php

--- data.php
+++ data.php
@@ -1,6 +1,36 @@
-<?php
-$data = [
-    ['id' => 1, 'task' => 'creating', 'date' => '2022-11-25', 'active' => 1],
-    ['id' => 2, 'task' => 'sample', 'date' => '2022-11-15', 'active' => 1],
-    ['id' => 3, 'task' => 'testing', 'date' => '2022-11-08', 'active' => 0],
-];
+<?php $data = [[
+            'id' => 1,
+            'task' => 'creating',
+            'date' => '2022-11-25',
+            'active' => 1,
+        ],[
+            'id' => 2,
+            'task' => 'Zody',
+            'date' => '2022-11-15',
+            'active' => 1,
+        ],[
+            'id' => 3,
+            'task' => 'testing',
+            'date' => '2022-11-08',
+            'active' => 0,
+        ],[
+            'id' => 4,
+            'task' => 'Neu',
+            'date' => '2022-12-03',
+            'active' => 1,
+        ],[
+            'id' => 5,
+            'task' => 'Neu2',
+            'date' => '2022-12-03',
+            'active' => 1,
+        ],[
+            'id' => 6,
+            'task' => 'Neu',
+            'date' => '2022-12-03',
+            'active' => 1,
+        ],[
+        'id' => 7,
+        'task' => 'Neu4',
+        'date' => '2022-12-03',
+        'active' => 1,
+    ],];
