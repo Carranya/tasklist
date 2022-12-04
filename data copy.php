4e2ecb3c9c4e0110ad32815772701feec03207a0
Carranya <k.gong82@gmail.com>
2022-12-04 06:55:30 +0100

    archived

---
+++ data copy.php
@@ -0,0 +1,6 @@
+<?php
+$data = [
+    ['id' => 1, 'task' => 'creating', 'date' => '2022-11-25', 'active' => 1],
+    ['id' => 2, 'task' => 'sample', 'date' => '2022-11-15', 'active' => 1],
+    ['id' => 3, 'task' => 'testing', 'date' => '2022-11-08', 'active' => 0],
+];
