54fa0a43ff3ce154b21e5194a3b6da9014e870ec
Carranya <k.gong82@gmail.com>
2022-12-05 07:53:50 +0100

    new functions

--- pages/main.php
+++ pages/main.php
@@ -4,6 +4,18 @@
 if(isset($_POST['create'])){
     create($_POST['newTask']);
 }
+
+if(isset($_POST['edit'])){
+    edit($_POST['edit'], $_POST['taskEdit']);
+}
+
+if(isset($_POST['done'])){
+    done($_POST['done']);
+}
+
+if(isset($_POST['undone'])){
+    undone($_POST['undone']);
+}
     require('data.php');
     include 'pages/showList.php';
 ?>
