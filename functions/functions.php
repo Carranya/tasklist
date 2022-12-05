1dedf6604a4143bcf66e8236ddb313a614740d73
Carranya <k.gong82@gmail.com>
2022-12-05 07:53:48 +0100

    new functions

--- functions/functions.php
+++ functions/functions.php
@@ -34,3 +34,75 @@ function create($task){
     $content .= "];";
     createData($content);
 }
+
+function edit($id, $task){
+    global $data;
+    $lists = $data;
+    $content = '<?php $data = [';
+    foreach($lists as $list){
+        $content .= "[
+            'id' => " . $list['id'] . ",";
+        if($list['id'] == $id){
+            $content .= "
+            'task' => '$task',";
+        } else {
+            $content .= "
+            'task' => '" . $list['task'] . "',";
+        }
+        $content .= "
+            'date' => '" . $list['date'] . "',
+            'active' => " . $list['active'] . ",
+    ],";
+    }
+    $content .= "];";
+    createData($content);
+}
+
+function done($id){
+    global $data;
+    $lists = $data;
+    $content = '<?php $data = [';
+    foreach($lists as $list){
+        $content .= "[
+            'id' => " . $list['id'] . ",
+            'task' => '" . $list['task'] . "',
+            'date' => '" . $list['date'] . "',";
+          
+        if($list['id'] == $id){
+            $content .= "
+            'active' => 0,";
+        } else {
+            $content .= "
+            'active' => " . $list['active'] . ",";
+        }
+        $content .= "
+    ],";
+    }
+    $content .= "];";
+    createData($content);
+}
+
+function undone($id){
+    global $data;
+    $lists = $data;
+    $content = '<?php $data = [';
+    foreach($lists as $list){
+        $content .= "[
+            'id' => " . $list['id'] . ",
+            'task' => '" . $list['task'] . "',
+            'date' => '" . $list['date'] . "',
+            ";
+
+        if($list['id'] == $id){
+            $content .= "
+            'active' => 1,";
+        } else {
+            $content .= "
+            'active' => " . $list['active'] . ",";
+        }
+        $content .= "
+        ],";
+    }
+    $content .= "];";
+    createData($content);
+}
