d82c7e7616f7feea37ea44e5fd572dfe33689d82
Carranya <k.gong82@gmail.com>
2022-12-06 06:39:15 +0100

    add title

--- buttons/cancel.php
+++ buttons/cancel.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button><img src='img/close.png' class='w-7 h-7 hover:opacity-40'></button>";
+echo "<button><img src='img/close.png' class='w-7 h-7 hover:opacity-40' title='abbrechen'></button>";
 ?>
