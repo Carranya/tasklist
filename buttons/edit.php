069dd08e7352b0c1f4a782362217f1e4fef37315
Carranya <k.gong82@gmail.com>
2022-12-06 06:39:29 +0100

    add title

--- buttons/edit.php
+++ buttons/edit.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='edit' value='" . $list['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40'></button>";
+echo "<button name='edit' value='" . $list['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
 ?>
