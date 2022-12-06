4051f9322c0bc554758e975c3318a80e4ac75d0e
Carranya <k.gong82@gmail.com>
2022-12-06 06:39:19 +0100

    add title

--- buttons/create.php
+++ buttons/create.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='create' value='1'><img src='img/done.png' class='w-7 h-7 hover:opacity-40'></button>";
+echo "<button name='create' value='1'><img src='img/create.png' class='w-7 h-7 hover:opacity-40' title='hinzufügen'></button>";
 ?>
