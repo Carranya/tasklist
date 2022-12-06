3d0a473393898e64e3f835fcbac150e70da734d9
Carranya <k.gong82@gmail.com>
2022-12-06 06:40:03 +0100

    add functions

--- pages/showList.php
+++ pages/showList.php
@@ -1,12 +1,28 @@
 <div class='inline-block w-96 border border-blue-300 rounded-xl text-left p-3 m-5 bg-blue-300 shadow-xl'>
-    <h1 class='text-center text-2xl font-bold underline mb-3'>Aufgabenliste</h1>
+    <span class='float-right'>
+    <?php
+        if(isset($_POST['showAll'])){
+            $showAll = true;
+            echo "<input type='hidden' name='showAll' value=true>";
+        }
+
+        if(isset($showAll)){
+            include 'buttons/showActive.php';
+        } else {
+            include 'buttons/showAll.php';
+        }
+        ?>
+    </span>
+    <h1 class='text-center text-2xl font-bold underline mb-3 mr-3'>Aufgabenliste</h1>
 
     <?php
         global $data;
         $step = 0;
-        $showAll = true;
-        // unset($showAll);
         foreach($data as $list){
+            if($list['id'] == -100){
+                continue;
+            }
+
             if(!isset($showAll)){
                 if($list['active'] == 0){
                     continue;
@@ -15,26 +31,27 @@
 
             if($step == 0){
                 $bg = 'bg-blue-200';
-                $step--;
+                $step++;
             } else {
                 $bg = 'bg-blue-100';
-                $step++;
+                $step--;
             }
 
             $date = new \DateTimeImmuTable($list['date']);
-            $showDate = $date->format('d. M Y');
+            $showDate = $date->format('d. M. Y');
             if($list['active'] == 0){
                 echo "<div class='p-3 " . $bg . " flex justify-between items-center opacity-40'>"; 
             } else {
                 echo "<div class='p-3 " . $bg . " flex justify-between items-center'>";
             }
-                echo "<span>" . $list['id'] . "</span>";
-                if(@$_POST['pickToEdit'] == $list['id']){
-                    echo "<input name='taskEdit' value='" . $list['task'] . "' size='10'>";
-                } else {
-                   echo "<span>" . $list['task'] . "</span>";
-                }
-                echo "<span>" . $showDate . "</span>";
+
+            if(@$_POST['pickToEdit'] == $list['id']){
+                echo "<input name='taskEdit' value='" . $list['task'] . "' size='10'>";
+            } else {
+               echo "<span>" . $list['task'] . "</span>";
+            }
+            
+            echo "<span>" . $showDate . "</span>";
 
             if($list['active'] == 0){
                 echo "<div class='flex'>";
