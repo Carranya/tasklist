3017a6fa8c72f37feee8938529f82c69993152ad
Carranya <k.gong82@gmail.com>
2022-12-06 06:39:32 +0100

    add title

--- buttons/pickToEdit.php
+++ buttons/pickToEdit.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='pickToEdit' value='" . $list['id'] . "'><img src='img/edit.png' class='w-7 h-7 hover:opacity-40'></button>";
+echo "<button name='pickToEdit' value='" . $list['id'] . "'><img src='img/edit.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
 ?>
