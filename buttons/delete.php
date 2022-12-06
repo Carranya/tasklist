95a81f4ec18089a4693c907a2aaeb524e88650e1
Carranya <k.gong82@gmail.com>
2022-12-06 06:39:22 +0100

    add title

--- buttons/delete.php
+++ buttons/delete.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='delete' value='" . $list['id'] . "'><img src='img/delete.png' class='w-7 h-7 hover:opacity-40'></button>";
+echo "<button name='delete' value='" . $list['id'] . "'><img src='img/delete.png' class='w-7 h-7 hover:opacity-40' title='löschen'></button>";
 ?>
