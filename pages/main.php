dcc2ccf534cb7372f8afbed2c102f847b22c5634
Carranya <k.gong82@gmail.com>
2022-12-12 10:26:33 +0100

    updated functions

--- pages/main.php
+++ pages/main.php
@@ -1,24 +1,24 @@
 <form action='index.php' method='post'>
 <?php
 
-if(isset($_POST['create'])){
-    create($_POST['newTask']);
+if(isset($_POST['done'])){
+    done($_POST['done'], 0);
+}
+
+if(isset($_POST['undone'])){
+    done($_POST['undone'], 1);
+}
+
+if(isset($_POST['delete'])){
+    done($_POST['delete'], 2);
 }
 
 if(isset($_POST['edit'])){
     edit($_POST['edit'], $_POST['taskEdit']);
 }
 
-if(isset($_POST['done'])){
-    done($_POST['done']);
-}
-
-if(isset($_POST['undone'])){
-    undone($_POST['undone']);
-}
-
-if(isset($_POST['delete'])){
-    delete($_POST['delete']);
+if(isset($_POST['create'])){
+    create($_POST['newTask']);
 }
     require('data.php');
     include 'pages/showList.php';
