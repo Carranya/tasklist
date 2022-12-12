419c8142b86471157c4c205329f49b323def76d4
Carranya <k.gong82@gmail.com>
2022-12-12 10:26:20 +0100

    updated functions

--- buttons/done.php
+++ buttons/done.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='done' value='" . $list['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40' title='erledigt'></button>";
+echo "<button name='done' value='" . $dat['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40' title='erledigt'></button>";
 ?>
