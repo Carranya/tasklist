80afbbe7893d1229d92500f70e8136248a50eaa4
Carranya <k.gong82@gmail.com>
2022-12-12 10:26:25 +0100

    updated functions

--- buttons/pickToEdit.php
+++ buttons/pickToEdit.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='pickToEdit' value='" . $list['id'] . "'><img src='img/edit.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
+echo "<button name='pickToEdit' value='" . $dat['id'] . "'><img src='img/edit.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
 ?>
