41f2e39040dc3b64dd5c839a40f4fbf1877d24fc
Carranya <k.gong82@gmail.com>
2022-12-12 10:26:28 +0100

    updated functions

--- buttons/undone.php
+++ buttons/undone.php
@@ -1,3 +1,3 @@
 <?php
-    echo "<button name='undone' value='" . $list['id'] . "'><img src='img/close.png' class='w-7 h-7 hover:opacity-40' title='rückgängig'></button>";
+    echo "<button name='undone' value='" . $dat['id'] . "'><img src='img/close.png' class='w-7 h-7 hover:opacity-40' title='rückgängig'></button>";
 ?>
