20e4a09deca343cd3f93efac5ee390e0a5909a6b
Carranya <k.gong82@gmail.com>
2022-12-12 10:26:23 +0100

    updated functions

--- buttons/edit.php
+++ buttons/edit.php
@@ -1,3 +1,3 @@
 <?php
-echo "<button name='edit' value='" . $list['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
+echo "<button name='edit' value='" . $dat['id'] . "'><img src='img/done.png' class='w-7 h-7 hover:opacity-40' title='ändern'></button>";
 ?>
